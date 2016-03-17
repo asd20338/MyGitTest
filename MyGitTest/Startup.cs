27480bd7b36e7da57aaf1d789fee00d058bda061
HongBin Chen <asd203387@hotmail.com>
2016-03-17 10:42:00 +0800

    create

---
+++ MyGitTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyGitTest.Startup))]
+namespace MyGitTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
